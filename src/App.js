386b7a1e4f1b74886e5d52457036e3e8995e49dd
Luka France <lukafrance@gmail.com>
2022-05-19 15:34:59 +0100

    luka first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import CardSmall from './CardSmall';
+import CardLarge from './CardLarge';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-
-
-
-
+      <CardSmall 
+    catImage = 'https://27.media.tumblr.com/tumblr_lt9rfcQo7u1r4xjo2o1_1280.jpg' // API Image link will go here
+    catTitle = 'CAT NAME' // Cat Name
+    catDesc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla lectus hfduaisofhudsaiofhasuihduiafohudsaiofh'
+    catPrice = '£1200'
+   />
+   <CardLarge 
+   catImage = 'https://27.media.tumblr.com/tumblr_lt9rfcQo7u1r4xjo2o1_1280.jpg' // API Image link will go here
+   catTitle = 'CAT NAME' // Cat Name
+   catDesc = 'Lorem ipsum dolor sit amet, consectetur  porttitor commodo'
+   catPrice = '£800'
+   />
     </div>
   );
 }
--- src/CardLarge.js
+++ src/CardLarge.js
@@ -6,6 +6,11 @@ const CardLarge = (props) => {
 
     return (
         <DivStyled>
+    <img alt='' src={props.catImage} />
+    <h2>{props.catTitle}</h2>
+    <p>{props.catDesc}</p>
+    <p1>{props.catPrice}</p1>
+    <button>Add to cart</button>
 
         </DivStyled>
     )
@@ -16,4 +21,34 @@ export default CardLarge
 
 const DivStyled = styled.div`
 
-`
+border: 2px solid grey;
+width: 50vh;
+height: 75vh;
+margin: 20px;
+background-color: #61dafb;
+border-radius: 10px;
+box-shadow: 2.5px 5px #000000;
+
+ & p1 {
+    border: 3px solid grey;
+     padding: 0rem;
+     border: 1px solid grey;
+     margin: 20px;
+    
+     
+ }
+& h2 {
+    margin: 0rem;
+    border: 1px solid grey;
+
+ 
+}
+
+
+& img {
+    padding: 5px;
+    width: 18rem;
+    margin: 2rem;
+    border-radius: 20px;
+    border: 0px solid grey;
+}`
