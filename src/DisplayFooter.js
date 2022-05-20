6e8824611385251d93faa26296d43841dd518213
Mohammed <h-qureshi95@hotmail.co.uk>
2022-05-20 13:59:47 +0100

    mohammed commit friday

--- src/DisplayFooter.js
+++ src/DisplayFooter.js
@@ -1,11 +1,26 @@
 import styled from "styled-components";
+import {FaTwitter} from "react-icons/fa"
+import {FaFacebook} from "react-icons/fa"
+import {FaInstagram} from "react-icons/fa"
+import {FaSnapchat} from "react-icons/fa"
 
 const DisplayFooter = (props) => {
 
 
     return (
         <DivStyled>
-
+                <p style={{fontSize:"17px", width: "130px", alignItems: "center", marginLeft: "20px"}}>Cats4Lyf &copy; 2022</p>
+            <div style={footerText}>
+                <p style={footerTextStyle}>Privacy Statement |</p>
+                <p style={footerTextStyle}>Terms &amp; Conditions</p>
+            </div>
+            <div style={footerSocials}>
+                Follow us on
+                <FaTwitter />
+                <FaFacebook />
+                <FaInstagram />
+                <FaSnapchat />
+            </div>
         </DivStyled>
     )
 }
@@ -14,5 +29,28 @@ export default DisplayFooter
 
 
 const DivStyled = styled.div`
-
+    background-color: darkblue;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    color: lightblue
 `
+
+const footerText = {
+    display: "flex",
+    marginLeft: "100px",
+    fontSize: "17px",
+    alignItems: "center"
+}
+const footerTextStyle = {
+    marginLeft: "10px",
+}
+
+const footerSocials = {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "300px",
+    fontSize: "20px",
+    marginRight: "20px",
+    alignItems: "center"
+}
--- src/DisplayHeader.js
+++ src/DisplayHeader.js
@@ -1,11 +1,23 @@
 import styled from "styled-components";
+import {FaSearch} from "react-icons/fa"
 
 const DisplayHeader = (props) => {
 
 
     return (
         <DivStyled>
-
+            <h1 style={{marginLeft: "30px"}}>Cats4Lyf</h1>
+            <div className="nav-links" style={navbarLinks}>
+                <a href="#" style={navbarLinks}>About</a>
+                <a href="#" style={navbarLinks}>Services</a>
+                <a href="#" style={navbarLinks}>Products</a>
+                <a href="#" style={navbarLinks}>Contact</a>
+                <a href="#" style={navbarLinks}>Help</a>
+            </div>
+            <div>
+                <input className= "search-bar" placeholder="Search..."></input>
+                <button className = "search-button" style={searchButton}><FaSearch /></button>
+            </div>
         </DivStyled>
     )
 }
@@ -14,5 +26,33 @@ export default DisplayHeader
 
 
 const DivStyled = styled.div`
+height: 100px;
+width: 100%;
+background-color: darkblue;
+color: lightblue;
+display: flex;
+justify-content: space-between;
+align-items: center;
+font-size: 24px;
+margin: 20px 0 20px 0;
 
 `
+
+const navbarLinks = {
+    display: "flex",
+    width: "840px",
+    justifyContent: "space-between",
+    textDecoration: "none",
+}
+
+
+
+const searchButton = {
+    fontSize: "20px",
+    padding: "5px",
+    borderRadius: "10px",
+    marginLeft: "10px",
+    marginRight: "10px",
+    border: "none",
+    backgroundColor: "lightblue"
+}
